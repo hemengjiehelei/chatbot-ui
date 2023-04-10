a88c61120335d1e9b5647a0d4244704a772c17d3
hemengjiehelei <453350925@qq.com>
2023-04-10 15:14:57 +0800

    Update const.ts

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_SYSTEM_PROMPT =
-  "You need to apply for an account and send it to 689623@cimc.com";
+  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
